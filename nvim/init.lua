14e79215cf7d81c51cbd6688f1f1075d1b0edf89
EBrain <ebrain.ebrain@gmail.com>
2022-10-04 23:47:35 +0630

    add windows clipboard support & update coda_action shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,8 +9,11 @@ local has = function(x)
 end
 
 local is_mac = has "macunix"
--- local is_win = has "win32"
+local is_win = has "win32"
 
 if is_mac then
   require('macos')
+end
+if is_win then
+  require('windows')
 end
--- nvim/plugin/lspsaga.lua
+++ nvim/plugin/lspsaga.lua
@@ -11,6 +11,6 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<Leader>er', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('n', 'ga', '<Cmd>Lspsaga code_action<CR>', opts)
+vim.keymap.set('n', '<Leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<Leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
