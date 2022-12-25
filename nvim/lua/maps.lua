abcdf3866ba7f090202d2efbf4da6af9b0752b08
EBrain <ebrain.ebrain@gmail.com>
2022-12-25 17:33:17 +0630

    Add new keymaps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,23 +1,26 @@
 local keymap = vim.keymap
 
+keymap.set('n', '<Leader>o', 'o<Esc>', { noremap = true })
+keymap.set('n', '<Leader>O', 'O<Esc>', { noremap = true })
+
 -- Do not yank
-keymap.set('n', 'x', '"_x')
-keymap.set('n', 'c', '"_c')
+keymap.set('n', 'x', '"_x', { noremap = true })
+keymap.set('n', 'c', '"_c', { noremap = true })
 
 -- Vertical Movement with center
-keymap.set('n', '<C-u>', "<C-u>zz")
-keymap.set('n', '<C-d>', "<C-d>zz")
+keymap.set('n', '<C-u>', "<C-u>zz", { noremap = true })
+keymap.set('n', '<C-d>', "<C-d>zz", { noremap = true })
 
-keymap.set('n', 'n', "nzzzv")
-keymap.set('n', 'N', "nzzzv")
+keymap.set('n', 'n', "nzzzv", { noremap = true })
+keymap.set('n', 'N', "nzzzv", { noremap = true })
 
-keymap.set('n', 'J', "mzJ`z") -- restore cursor location
+keymap.set('n', 'J', "mzJ`z", { noremap = true }) -- restore cursor location
 
 -- clipboards
-keymap.set('n', 'Y', "y$") -- copy from cursor to end of line
-keymap.set('n', '<Leader>y', '"+y')
-keymap.set('n', '<Leader>Y', '"+y$') -- copy from cursor to end of line
-keymap.set('v', '<Leader>y', '"+y')
+keymap.set('n', 'Y', "y$", { noremap = true }) -- copy from cursor to end of line
+keymap.set('n', '<Leader>y', '"+y', { noremap = true })
+keymap.set('n', '<Leader>Y', '"+y$', { noremap = true }) -- copy from cursor to end of line
+keymap.set('v', '<Leader>y', '"+y', { noremap = true })
 
 -- Increment/decrement
 keymap.set('n', '+', '<C-a>')
@@ -29,13 +32,24 @@ keymap.set('n', '<Leader>st', ':tabedit<Return>', { silent = true })
 -- Split windows
 keymap.set('n', '<Leader>ss', ':split<Return><C-w>w', { silent = true })
 keymap.set('n', '<Leader>sv', ':vsplit<Return><C-w>w', { silent = true })
-
 keymap.set('n', '<Leader>sm', ':vsplit<Return><C-w>w:vertical resize 60<Return>:set nornu<Return>:set nonu<Return>',
   { silent = true })
+
+-- Resize windows
+keymap.set('n', '<C-Up>', ':resize -2<Return>', { silent = true })
+keymap.set('n', '<C-Down>', ':resize +2<Return>', { silent = true })
+keymap.set('n', '<C-Left>', ':vertical resize -2<Return>', { silent = true })
+keymap.set('n', '<C-Right>', ':vertical resize +2<Return>', { silent = true })
 
 -- Move window
 keymap.set('n', '<Leader>n', '<C-w>w')
 
 -- Move line
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
+
+-- Insert keymap
+-- keymap.set('i', '<Char-059><Char-059>', '<Esc>A;<Esc>', { noremap = true })
+keymap.set('i', ';;', '<Esc>A;<Esc>', { noremap = true })
+
+-- Ctrl + Enter in insert mode
