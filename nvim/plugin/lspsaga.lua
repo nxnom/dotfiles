b201f1069670b6b540c3ebdc25fee0a872800cd5
EBrain <ebrain.ebrain@gmail.com>
2023-01-20 22:26:07 +0630

    Update lspsaga configs due to breaking changes

--- nvim/plugin/lspsaga.lua
+++ nvim/plugin/lspsaga.lua
@@ -1,7 +1,7 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.init_lsp_saga {
+saga.setup {
   finder_request_timeout = 8000,
   rename_in_select = false,
   code_action_lightbulb = {
@@ -13,6 +13,14 @@ saga.init_lsp_saga {
     sign_priority = -1,
     virtual_text = true,
   },
+  ui = {
+    title = true,
+    border = 'single',
+    winblend = 0,
+    colors = {
+      normal_bg = '#1c1c19',
+    }
+  }
   -- server_filetype_map = {
   --   typescript = 'typescript'
   -- }
@@ -27,5 +35,5 @@ vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', '<Leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<Leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
-vim.keymap.set('n', '""', '<Cmd>Lspsaga open_floaterm<CR>', opts)
-vim.keymap.set('t', '""', '<Cmd>Lspsaga close_floaterm<CR>', opts)
+vim.keymap.set('n', '""', '<Cmd>Lspsaga term_toggle<CR>', opts)
+vim.keymap.set('t', '""', '<Cmd>Lspsaga term_toggle<CR>', opts)
