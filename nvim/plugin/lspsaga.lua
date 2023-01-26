e5878268a07c433ca0220465eac73e55e9df2811
EBrain <ebrain.ebrain@gmail.com>
2023-01-26 20:05:56 +0630

    Update lspsaga config

--- nvim/plugin/lspsaga.lua
+++ nvim/plugin/lspsaga.lua
@@ -2,28 +2,50 @@ local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
 saga.setup {
-  finder_request_timeout = 8000,
-  rename_in_select = false,
-  code_action_lightbulb = {
+  request_timeout = 8000,
+  lightbulb = {
     enable = true,
     enable_in_insert = true,
     cache_code_action = true,
     sign = true,
     update_time = 150,
     sign_priority = -1,
-    virtual_text = true,
+    -- virtual_text = true,
+  },
+  symbol_in_winbar = {
+    enable = false,
   },
   ui = {
-    title = true,
+    -- title = true,
     border = 'single',
     winblend = 0,
     colors = {
       normal_bg = '#1c1c19',
     }
-  }
-  -- server_filetype_map = {
-  --   typescript = 'typescript'
-  -- }
+  },
+  outline = {
+    win_position = 'right',
+    win_with = '',
+    win_width = 30,
+    show_detail = true,
+    auto_preview = true,
+    auto_refresh = true,
+    auto_close = true,
+    custom_sort = nil,
+    keys = {
+      jump = 'o',
+      expand_collapse = 'u',
+      quit = 'q',
+    },
+  },
+  rename = {
+    quit = '<C-c>',
+    exec = '<CR>',
+    mark = 'x',
+    confirm = '<CR>',
+    in_select = true,
+    whole_project = true,
+  },
 }
 
 -- noremap = true,
@@ -35,5 +57,6 @@ vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', '<Leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<Leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', '<Leader>oo', '<Cmd>Lspsaga outline<CR>', opts)
 vim.keymap.set('n', '""', '<Cmd>Lspsaga term_toggle<CR>', opts)
 vim.keymap.set('t', '""', '<Cmd>Lspsaga term_toggle<CR>', opts)
