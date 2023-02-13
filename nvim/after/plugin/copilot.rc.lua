537447a12db1c8bf16a1ddd5f3a12ba5e56f4dfa
EBrain <ebrain.ebrain@gmail.com>
2023-02-13 22:14:28 +0630

    Add copilot enable, disable shortcuts

--- nvim/after/plugin/copilot.rc.lua
+++ nvim/after/plugin/copilot.rc.lua
@@ -11,3 +11,5 @@ vim.g.copilot_filetypes = {
 }
 
 vim.keymap.set('n', '<C-\\>', ':Copilot panel<Return>', { silent = true })
+vim.keymap.set('n', '<leader>cd', ':Copilot disable<Return>', { silent = true })
+vim.keymap.set('n', '<leader>ce', ':Copilot enable<Return>', { silent = true })
