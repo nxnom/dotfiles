ad5b292559f4d078d537944b1b0e9ff54f4180da
EBrain <ebrain.ebrain@gmail.com>
2023-03-10 00:12:59 +0630

    Enable Copilot for all languages

--- nvim/after/plugin/copilot.rc.lua
+++ nvim/after/plugin/copilot.rc.lua
@@ -1,14 +1,16 @@
 vim.g.copilot_filetypes = {
-  ["*"] = false,
-  lua = true,
-  html = true,
-  css = true,
-  javascript = true,
-  typescript = true,
-  dart = true,
-  ruby = true,
-  zsh = true,
-  vue = true,
+  ["*"] = true,
+  -- ["*"] = false,
+  -- rust = true,
+  -- lua = true,
+  -- html = true,
+  -- css = true,
+  -- javascript = true,
+  -- typescript = true,
+  -- dart = true,
+  -- ruby = true,
+  -- zsh = true,
+  -- vue = true,
 }
 
 vim.keymap.set('n', '<C-\\>', ':Copilot panel<Return>', { silent = true })
