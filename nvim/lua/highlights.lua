c264c6a18b105827d8468bd1111ecad6f94b81c7
EBrain <ebrain.ebrain@gmail.com>
2023-03-10 16:41:15 +0630

    Change colorscheme to catppuccin-macchiato' '

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -5,6 +5,4 @@ vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
 
-vim.cmd [[colorscheme catppuccin-mocha]]
--- vim.cmd [[colorscheme tokyonight-night]]
--- vim.cmd [[colorscheme github_dark_default]]
+vim.cmd [[colorscheme catppuccin-macchiato]]
