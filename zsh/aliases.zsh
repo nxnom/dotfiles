bde167de47f52e764bae1dc0da1cc9d2e99403e8
oyhpnayiaw <ebrain.ebrain@gmail.com>
2023-10-04 17:27:11 +0630

    Create alias to start `scrcpy`

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,6 @@ alias cdt='cd ~/Temp'
 alias sls='live-server .'
 alias c=clear
 alias pn=pnpm
+
+# set SCRCPY_ADB_PATH to the path of your adb binary to avoid conflicts
+alias start-scrcpy='ADB=$SCRCPY_ADB_PATH scrcpy --always-on-top'
