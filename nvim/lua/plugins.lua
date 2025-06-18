e639d3549baff9d21443dc50440c985204ecb82c
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2025-06-18 14:37:19 +0700

    Update nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -115,6 +115,19 @@ local plugins = {
   -- AI
   'github/copilot.vim',
 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",                          -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   -- markdown previewer
   -- {
   --   "iamcco/markdown-preview.nvim",
